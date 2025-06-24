91a4ac8999d61ac88ce0175dba7cb7635490fb16
iamcarlogg <00052220@uca.edu.sv>
2025-06-24 09:48:51 -0600

    cambiando favoritos

--- marketuca/src/AppRouter.jsx
+++ marketuca/src/AppRouter.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from "./modules/home/page/home";
-import Dashboard from "./modules/dashboard/page/dashboard";
 import FavoritesPage from "./modules/dashboard/page/FavoritesPage";
 import Login from "./modules/login/page/login.jsx";
 import Register from "./modules/register/page/register.jsx";
